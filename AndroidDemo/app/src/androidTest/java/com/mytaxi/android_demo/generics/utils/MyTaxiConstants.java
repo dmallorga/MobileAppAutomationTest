ee3cd5cdeec739cf4f870972dddd9b784cae8e0b
Dmallorga <daniel.mallorga@infor.com>
2018-09-20 10:35:09 -0300

    Generics folder created for Constants.

---
+++ AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/generics/utils/MyTaxiConstants.java
@@ -0,0 +1,11 @@
+package com.mytaxi.android_demo.generics.utils;
+
+public class MyTaxiConstants {
+
+    public static final String MYTAXI_USERNAME = "crazydog335";
+    public static final String MYTAXI_PASSWORD = "venture";
+
+    public static final String MYTAXI_LOGOUT = "Logout";
+
+    public static final String MYTAXI_DRIVER_TO_SELECT = "Sarah Scott";
+}
